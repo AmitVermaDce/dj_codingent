b767988f66e9aa66284027f282225b2715c918df
AMIT VERMA <amitverma.dce@gmail.com>
2023-07-09 09:27:52 +0530

    Dynamic data rendering
    objects: 3 objects has been created
    Retrieving object: Articles.objects.get(id=int)
    Dynamic rendering of the data using id

--- config/views.py
+++ config/views.py
@@ -3,6 +3,7 @@ To render html pages
 '''
 from django.http import HttpResponse
 import random
+from articles.models import Article
 
 
 def home(request):
@@ -10,21 +11,9 @@ def home(request):
     Take in request (Django sends request)
     Return HTML as a response (We pick to return the response)
     '''
-    name = "John F Kennedy" # Hard Coded data
-    number = random.randint(1, 2344447) # pseudo random 
-
-    # from the database
-    H1_STRING = f'''
-    <h1>Hello "{name}" - {number}</h1>
-    '''
-    P_STRING = '''
-    <p>Dummy Text: 
-    <small>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-    It has survived not only five centuries, but also the leap into electronic typesetting, 
-    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
-    like Aldus PageMaker including versions of Lorem Ipsum.</small></p>
-    ''' 
-    return HttpResponse(H1_STRING +"\n" + P_STRING)
+    random_id = random.randint(1,3)
+    article_obj = Article.objects.get(id=random_id)
+    H1_STRING = f"<h1>{article_obj.title}</h1>"
+    P_STRING = f"<p>{article_obj.content}</p>" 
+    F_STRING = f"<i>Object ID: {article_obj.id}</i>"
+    return HttpResponse(H1_STRING +"\n" + P_STRING + "\n" + F_STRING)
