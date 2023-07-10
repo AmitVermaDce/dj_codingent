15f0104a1a196510d46fb4e8846205f34f6b1da9
AMIT VERMA <amitverma.dce@gmail.com>
2023-07-10 23:54:28 +0530

    Admin register with fields modification
    Admin Register:
    admin.site.register(Article)

--- articles/admin.py
+++ articles/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "content"]
+    search_fields = ["content"]
 
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
