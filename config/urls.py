7eca79bf2fb21386557bffcc70dfbc74e1fec9b8
AMIT VERMA <amitverma.dce@gmail.com>
2023-07-12 18:08:25 +0530

    Created Login Form with authentication & redirection
    - Registered an app "accounts".
    - Created a Login form for login and redirecting to a specific page.

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_view
-from articles.views import *
+from articles.views import (
+    article_search_view,
+    article_detail_view,
+    article_create_view,
+)
+from accounts.views import (
+    login_view,
+    logout_view,
+    register_view,
+)
 
 urlpatterns = [
-    path('', home_view, name="home"),
+    path('', home_view),
     path('articles/', article_search_view),
     path('articles/create/', article_create_view),
     path('articles/<int:id>/', article_detail_view),    
     path('admin/', admin.site.urls),
+    path('accounts/login/', login_view)
 ]
