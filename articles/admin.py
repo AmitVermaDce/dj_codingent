c0a5e44113b0ec5c3544c96f262573d3bb9b96a2
AMIT VERMA <amitverma.dce@gmail.com>
2023-07-16 10:01:12 +0530

    Making changes to models & Fields
    Added 3 fields
    - timestamp with auto_now_add(when the model added).
    - updated with auto_now(when the model updated).
    - publish with auto_now, auto_now_add, null, blank.

--- articles/admin.py
+++ articles/admin.py
@@ -3,7 +3,7 @@ from .models import Article
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "content"]
+    list_display = ["id", "title", "content", "timestamp", "updated", "publish"]
     search_fields = ["content"]
 
 admin.site.register(Article, ArticleAdmin)
--- articles/models.py
+++ articles/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=220)
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    publish = models.DateField(null=True, blank=True, auto_now=False, auto_now_add=False)
