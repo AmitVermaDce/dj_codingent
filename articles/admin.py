f312d58574a3dcd524eab7da325e34ea0c588dec
AMIT VERMA <amitverma.dce@gmail.com>
2023-07-16 17:29:34 +0530

    SlugField & Override Save Method
    Here we are slugifying a text using below function:
    - python manage.py shell
    ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
    from django.utils.text import slugify
    slugify("Hello world this is amazing!!! $#@")
    
    Output: Hello-world-this-is-amazing
    ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
    - Also overriding the save function

--- articles/admin.py
+++ articles/admin.py
@@ -3,7 +3,7 @@ from .models import Article
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "content", "timestamp", "updated", "publish"]
+    list_display = ["id", "title", "slug", "content", "publish"]
     search_fields = ["content"]
 
 admin.site.register(Article, ArticleAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=220)
     content = models.TextField()
+    slug = models.SlugField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     publish = models.DateField(null=True, blank=True, auto_now=False, auto_now_add=False)
+
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
