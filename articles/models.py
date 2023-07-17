dd03a18d7f153f597d348fb11c364a8f172a7585
AMIT VERMA <amitverma.dce@gmail.com>
2023-07-17 11:53:52 +0530

    Some Test functions

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.text import slugify
 from django.db.models.signals import pre_save, post_save
 from .utils import slugify_instance_title
 
--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from articles.models import Article
+from django.utils.text import slugify
 
 # Create your tests here.
+
+class ArticleTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.number_of_objects = 5
+        for i in range(0, self.number_of_objects):
+            Article.objects.create(title="hello world", 
+                               content = "Something else")
+
+    def test_querset_exists(self):
+        qs = Article.objects.all()
+        self.assertTrue(qs.exists())
+
+    def test_queryset_count(self):
+        qs = Article.objects.all()
+        self.assertEqual(qs.count(), self.number_of_objects)
+
+    def test_the_dummy_unique_slug(self):
+        qs = Article.objects.exclude(slug__iexact='the-dummy')
+        for obj in qs:
+            title = obj.title
+            slug = obj.slug
+            slugfield_title = slugify(title)
+            self.assertNotEqual(slug, slugfield_title)
+
+    
