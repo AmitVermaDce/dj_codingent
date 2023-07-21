bcc6207b88052ba426a6f1cfc845a37b2d3eea8c
AMIT VERMA <amitverma.dce@gmail.com>
2023-07-22 00:05:07 +0530

    Method-1: Customizing form fields, widgets, etc.
    describing each field with attrs one at a time.

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Recipe, RecipeIngredient
 
-class RecipeForm(forms.ModelForm):
+class RecipeForm(forms.ModelForm):    
+    error_css_class = ""
+    required_css_class = 'require-field'
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control",
+        "placeholder": "Recipe Name"
+    }))
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "rows": 3
+    }))
 
     class Meta:
         model = Recipe
