f34454c1193a1ec1527c7917778fa6009f81bd48
AMIT VERMA <amitverma.dce@gmail.com>
2023-07-22 00:25:07 +0530

    Method-2: Using __init__ method on self.fields
    This method helps in reducing the redundant code by applying logic on self.fields by looping over each field of the forms class.

--- recipes/forms.py
+++ recipes/forms.py
@@ -4,18 +4,27 @@ from .models import Recipe, RecipeIngredient
 class RecipeForm(forms.ModelForm):    
     error_css_class = ""
     required_css_class = 'require-field'
-    name = forms.CharField(widget=forms.TextInput(attrs={
-        "class": "form-control",
-        "placeholder": "Recipe Name"
-    }))
-    description = forms.CharField(widget=forms.Textarea(attrs={
-        "class": "form-control",
-        "rows": 3
-    }))
+    # name = forms.CharField(widget=forms.TextInput(attrs={
+    #     "class": "form-control",
+    #     "placeholder": "Recipe Name"
+    # }))
+    # description = forms.CharField(widget=forms.Textarea(attrs={
+    #     "class": "form-control",
+    #     "rows": 3
+    # }))
 
     class Meta:
         model = Recipe
         fields = ["name", "description", "directions",]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[str(field)].widget.attrs.update(
+                {"class": "form-control-2",
+                 "placeholder": f"Recipe {field}"})
+            self.fields["description"].widget.attrs.update({"row": "3"})
+            self.fields["directions"].widget.attrs.update({"row": "5"})
 
 
 class RecipeIngredientForm(forms.ModelForm):
